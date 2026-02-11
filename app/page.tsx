c2ce7ff1d5896185e894fa19b57777304061dc34
Randy Oest <oest@amazingrando.com>
2026-02-11 08:42:34 -0500

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -13,11 +13,11 @@ export default function Home() {
 
       <div className="bg-deep-orange-light flex-1 text-center py-8 pt-[34.821428571%] md:pt-56 xl:pt-56 relative">
         <p className={cn(
-          "absolute top-4 md:top-10 left-0 w-full text-center z-50 text-deep-orange font-bold text-xl md:text-3xl tracking-widest"
+          "absolute top-4 md:top-8 left-0 w-full text-center z-50 text-deep-orange font-bold text-xl md:text-3xl tracking-widest"
         )}>NOW AVAILABLE</p>
         <Image src={BGImage} alt="" fill className="object-cover hidden md:block" />
         <Image src={BGImageMobile} alt="" className="block absolute top-0 left-0 w-full md:hidden" />
-        <Headline className="absolute top-[7.5%] sm:top-[10%] md:top-[14.5%] left-0 right-0 w-full max-w-[850px] mx-auto z-40 text-navy px-4" />
+        <Headline className="absolute top-[7.5%] sm:top-[10%] md:top-[13.5%] left-0 right-0 w-full max-w-[850px] mx-auto z-40 text-navy px-4" />
         <HomeData />
       </div>
     </>
